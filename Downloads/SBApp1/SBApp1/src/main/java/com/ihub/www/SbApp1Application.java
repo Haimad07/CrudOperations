05b6581ea9d1ce40d194bc1f4e215c7b73c0f254
user4 <haimadmd07@gmail.com>
2024-01-08 10:47:13 +0530

    commited

---
+++ Downloads/SBApp1/SBApp1/src/main/java/com/ihub/www/SbApp1Application.java
@@ -0,0 +1,14 @@
+package com.ihub.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbApp1Application.class, args);
+		System.out.println("I Love SpringBoot");
+	}
+
+}

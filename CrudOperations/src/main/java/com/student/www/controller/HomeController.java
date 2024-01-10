be40311418db8b4581e89910bf98f54735455ad4
user4 <haimadmd07@gmail.com>
2024-01-10 09:48:15 +0530

    added  end points of save,get,update

--- CrudOperations/src/main/java/com/student/www/controller/HomeController.java
+++ CrudOperations/src/main/java/com/student/www/controller/HomeController.java
@@ -3,8 +3,11 @@ package com.student.www.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,10 +30,23 @@ public class HomeController {
 		return student;
 		
 	}
-	@GetMapping("/getAllStudents")
+	@GetMapping("/getAllStudent")
 	public List<Student> getAll(){
 		List<Student> studentList = studentRepository.findAll();
 		return studentList;
+	}
+	
+	@DeleteMapping("/deleteStudent/{id}")
+	public String deleteStundent(@PathVariable int id) {
+		Student student = studentRepository.findById(id).get();
+		if(student!=null)
+			studentRepository.delete(student);
+		return "Deleted Successfully!!";
+	}
+	@PutMapping("/UpdateData")
+	public Student updateStudentData(@RequestBody Student student) {
+		studentRepository.save(student);
+		return student;
 	}
 
 }  

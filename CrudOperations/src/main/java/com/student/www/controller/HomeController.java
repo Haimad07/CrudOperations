84f3e3a213bed7535dce7ef5d687f09de2856354
user4 <haimadmd07@gmail.com>
2024-01-10 07:28:02 +0530

    added getmapping

--- CrudOperations/src/main/java/com/student/www/controller/HomeController.java
+++ CrudOperations/src/main/java/com/student/www/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.student.www.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +26,11 @@ public class HomeController {
 		studentRepository.save(student);
 		return student;
 		
+	}
+	@GetMapping("/getAllStudents")
+	public List<Student> getAll(){
+		List<Student> studentList = studentRepository.findAll();
+		return studentList;
 	}
 
 }  
